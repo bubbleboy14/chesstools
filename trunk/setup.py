b2561117c2ba6d912aaea1274a9c39e882b2c482
mario.balibrera <mario.balibrera@7da554a8-8c1a-11dd-a14b-9ba93c563a2f>
2008-10-17 09:49:32 +0000

    fixed en passant bug

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chesstools',
-    version='0.1.2',
+    version='0.1.3',
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
