5075d009de34ecc3999fe2d4cc374a67fa4d9ff0
mario.balibrera@gmail.com <mario.balibrera@gmail.com@7da554a8-8c1a-11dd-a14b-9ba93c563a2f>
2013-03-15 04:37:57 +0000

    0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chesstools',
-    version='0.1.6',
+    version='0.1.7',
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
