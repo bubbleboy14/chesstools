8fe1d211f470233a4ed30be3e3f995c759ebaecc
bubbleboy14 <mario.balibrera@gmail.com>
2021-12-18 02:05:40 -0800

    pyjion support .... but it's slower lol

--- chesstools/__init__.py
+++ chesstools/__init__.py
@@ -1,8 +1,17 @@
 try:
-    import psyco
-    psyco.full()
+    import pyjion
+    if input("you have pyjion. it's probs not faster. use it anyway? [N/y] ").lower() == "y":
+        pyjion.enable()
+        print("pyjioning!!!")
+    else:
+        print("no pyjion speedup (will probs be faster anyway)")
 except:
-    print("install psyco to make your bot run much faster!")
+    try:
+        import psyco
+        psyco.full()
+        print("initializing psyco - wow, you must have python <2.7! how?")
+    except:
+        print("pyjion (py3.10+) or psyco (py<2.7) _may_ help your bot run faster")
 
 COLORS = {'white':'black','black':'white'}
 
