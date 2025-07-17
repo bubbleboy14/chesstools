0d436693c56faad1e7f7c9a2bef7292b098cad76
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-17 15:55:33 -0700

    trans batching - doesn't help much, still slow

--- chesstools/ai/transposition.py
+++ chesstools/ai/transposition.py
@@ -11,16 +11,12 @@ class Table(Loggy):
         self._all = {}
         self._deepest = {}
 
-    def add(self, trans):
-        sig = trans.sig
-        dtup = (trans.depth, trans.score)
-        if sig in self._deepest:
-            self._all[sig].append(trans)
-            if self._deepest[sig][0] < trans.depth:
-                self._deepest[sig] = dtup
-        else:
+    def add(self, sig, dtup):
+        if sig not in self._deepest or self._deepest[sig][0] < dtup[0]:
             self._deepest[sig] = dtup
-            self._all[sig] = [trans]
+        if sig not in self._all:
+            self._all[sig] = []
+        self._all[sig].append(dtup)
 
     def get(self, sig, depth):
         if sig in self._deepest:
@@ -32,14 +28,20 @@ class Table(Loggy):
             return (trans.depth, trans.score)
 
     def score(self, variation, score, depth):
+        self.add(variation.signature(), (depth, score))
+
+    def trans(self, sig, tup):
         trans = Transposition()
-        trans.depth = depth
-        trans.score = variation.score
-        trans.sig = variation.signature()
-        self.add(trans)
+        trans.depth = tup[0]
+        trans.score = tup[1]
+        trans.sig = sig
+        return trans
+
+    def transets(self, sig):
+        return list(map(lambda tup : self.trans(sig, tup), self._all[sig]))
 
     def flush(self):
-        transes = sum(self._all.values(), [])
+        transes = sum(list(map(self.transets, self._all.keys())), [])
         db.put_multi(transes)
         self._all = {}
-        self.log("flushed:", len(transes), "cache:", len(self._deepest.keys()))
+        self.log("saved:", len(transes), "cache:", len(self._deepest.keys()))
