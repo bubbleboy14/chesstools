2128b599b0d00a9600727120165a25deb7842e90
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-17 17:51:14 -0700

    ai.transpotition: Table add()/get()/score() support withdb (default False) kwarg

--- chesstools/ai/transposition.py
+++ chesstools/ai/transposition.py
@@ -15,24 +15,26 @@ class Table(Loggy):
         self._all = {}
         self._deepest = {}
 
-    def add(self, sig, dtup):
+    def add(self, sig, dtup, withdb=False):
         if sig not in self._deepest or self._deepest[sig][0] < dtup[0]:
             self._deepest[sig] = dtup
-        if sig not in self._all:
-            self._all[sig] = []
-        self._all[sig].append(dtup)
+        if withdb:
+            if sig not in self._all:
+                self._all[sig] = []
+            self._all[sig].append(dtup)
 
-    def get(self, sig, depth):
+    def get(self, sig, depth, withdb=False):
         if sig in self._deepest:
             dtup = self._deepest[sig]
             if dtup[0] >= depth:
                 return dtup
-        trans = Transposition.query(Transposition.sig == sig).order(-Transposition.depth).get()
-        if trans and trans.depth >= depth:
-            return (trans.depth, trans.score)
+        if withdb:
+            trans = Transposition.query(Transposition.sig == sig).order(-Transposition.depth).get()
+            if trans and trans.depth >= depth:
+                return (trans.depth, trans.score)
 
-    def score(self, variation, score, depth):
-        self.add(variation.signature(), (depth, score))
+    def score(self, variation, score, depth, withdb=False):
+        self.add(variation.signature(), (depth, score), withdb)
 
     def trans(self, sig, tup):
         trans = Transposition()
